646fcdf39b3955d84624e2b0e5b17d1ef0f9b593
Dhiraj Chhetri <43646697+dhirajchhetri@users.noreply.github.com>
2024-08-17 02:35:56 +0000

    even odd

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,24 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the integer");
+        int number=  scanner.nextInt();
+        System.out.println(isEven(number));
+        scanner.close();
     }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        return false;
+    };
 }
